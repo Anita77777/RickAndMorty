9dbff321942d86f7eaf1e1f8ec30e44a939a47ea
Mikail Sayan <msayan@outlook.de>
2022-04-08 16:15:28 +0200

    Edit filter

--- main.js
+++ main.js
@@ -24,13 +24,41 @@ function convertOurDataToHTML(profiles) {
     listElement.append(imgElement);
     unorderedList.append(listElement);
   });
+
+  const ramform = document.querySelector("[data-js='ramform']");
+  let currentFilter = 'all';
+
+  ramform.addEventListener("change", () => {
+    // Überschreibt den Value der ausgewählten Box in currentFilter
+    currentFilter = ramform.elements["ram-filter"].value;
+    filterCharacter();
+  })
+
+  function filterCharacter() {
+    const divResults = document.querySelector("[data-js='filterResult']");
+    divResults.innerHTML = ``;
+    const unorderedList = document.createElement("ul");
+    data.results
+      .filter(character => character.status.includes(currentFilter) || currentFilter === 'all')
+      .forEach(character => {
+        const listElement = document.createElement("li");
+        listElement.textContent = character.name;
+        const imgElement = document.createElement("img");
+        imgElement.setAttribute("src", character.image);
+        listElement.append(imgElement);
+        unorderedList.append(listElement);
+        divResults.append(unorderedList);
+      })
+  }
 }
 
 document.querySelector("#app").innerHTML = `
 <h1>Rick and Morty Profiles</h1>
-<form>
+<form data-js="ramform">
   <fieldset>
-    <input type="radio" name="ram-filter" value="alive" checked>
+    <input type="radio" name="ram-filter" value="all" checked>
+    <label>All</label>
+    <input type="radio" name="ram-filter" value="alive">
     <label>Alive</label>
     <input type="radio" name="ram-filter" value="dead">
     <label>Dead</label>
@@ -39,10 +67,18 @@ document.querySelector("#app").innerHTML = `
   </fieldset>
 </form>
 <button class="button" data-js="button__random">Random</button>
-
+<div data-js="filterResult"></div>
 `;
 
 const buttonR = document.querySelector('[data-js="button__random"]');
 buttonR.addEventListener("click", () => {
   fetchRam(url);
 });
+
+
+
+
+
+
+
+  
