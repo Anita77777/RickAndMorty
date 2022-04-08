508e8a67040a009725092933912fec7f8ea1ae17
Anita Savic <savicanita500@gmail.com>
2022-04-08 14:34:47 +0200

    Rick and Morty upload changed file

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
-import './style.css'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
+const url = "https://rickandmortyapi.com/api/character";
+
+async function fetchRam(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    convertOurDataToHTML(data.results);
+  } catch (error) {
+    console.error(`Upps das war ein Fehler: ${error}`);
+  }
+}
+
+fetchRam(url);
+
+function convertOurDataToHTML(profiles) {
+  const unorderedList = document.createElement("ul");
+  document.body.append(unorderedList);
+
+  profiles.forEach((character) => {
+    const listElement = document.createElement("li");
+    listElement.textContent = character.name;
+    unorderedList.append(listElement);
+  });
+}
+
+document.querySelector("#app").innerHTML = `
   <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+  
+`;
